2283a6c6c36dfc07b47302b152ab0a893bdea588
renanBotasse <renanBotasse@bitbucket.org>
2023-09-05 16:49:31 +0100

    start the frontend - create and read

---
+++ .history/app_20230905152336.js
@@ -0,0 +1,65 @@
+//link express
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const Book = require('./models/libraryModel');
+
+// Config .env file
+dotenv.config();
+
+//start express
+const app = express();
+
+// Config ejs (view engine)
+app.set('view engine', 'ejs');
+
+// Create client static files (/public/assets) 
+app.use(express.static('public'));
+
+//MongoDB connection
+let mongoDB = 'mongodb+srv://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@cluster0.7796qkc.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(mongoDB);
+
+//MongoDB connection Confirmation
+mongoose.connection.on('connected', () => {
+	console.log('Connected to MongoDB');
+});
+
+//MongoDB connection Error
+mongoose.connection.on('error', (err) => {
+    console.log('Database Error' + err);
+});
+
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+//valid end point - start with /library
+const routes = require('./routes/libraryRoute');
+app.use('/', routes);
+
+//MIDDLEWARE START----------------------------------------------------------------
+//active json parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//error handler for middleware
+app.use(function(err, req, res, next) {
+	console.log(err);
+	//Change the status (res.status(422)
+	res.status(422).send({error: err.message});
+});
+
+//MIDDLEWARE END-------------------------------------------------------------------
+
+//  Route Root
+app.get('/api', function(req, res) {
+  res.render("createBook", { Book });
+});
+
+//create server port
+let port = 8000;
+
+//start server
+app.listen(process.env.PORT || port, () => {
+  console.log('Server running on port' + port);
+});
