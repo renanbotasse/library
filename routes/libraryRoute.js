0ff59a54f9b3d27b0540e5259838c9844fd82a7e
renanBotasse <renanBotasse@bitbucket.org>
2023-09-05 21:12:10 +0100

    create, edit, home and read - need find to finish the basic frontend

--- routes/libraryRoute.js
+++ routes/libraryRoute.js
@@ -5,21 +5,27 @@ const router = express.Router();
 const libraryController = require('../controllers/libraryController.js');
 
 // TEST ('GET') - route (url) to test (http://localhost:5000/library/test)
-router.get('/test', libraryController.all);
+router.get('/newBook', libraryController.newBook);
 
 // ALL ('GET') - route to get all books (http://localhost:5000/library/my-library)
-router.get('/readAllBooks', libraryController.all);
+router.get('/read', libraryController.all);
+
+// ONE BOOK ('GET') - route to get one Book book (http://localhost:5000/library/oneBook)
+router.get('/find', libraryController.find);
 
 // ONE BOOK ('GET') - route to get one Book book (http://localhost:5000/library/oneBook)
 router.get('/oneBook', libraryController.oneBook);
 
 // CREATE ('POST')- route to add a book (http://localhost:5000/library/create)
-router.post('/create', libraryController.create);
+router.post('/createBook', libraryController.create);
+
+// EDIT ('GET')- route to update a book (http://localhost:5000/library/edit)
+ router.get('/edit/:id', libraryController.edit);
 
 // UPDATE ('PUT')- route to update a book (http://localhost:5000/library/update/:id)
-router.put('/update/:id', libraryController.update);
+router.post('/update/:id', libraryController.update);
 
 // DELETE ('DELETE') - route to delete a book (http://localhost:5000/library/delete/:id)
-router.delete('/delete/:id', libraryController.delete);
+router.get('/delete/:id', libraryController.delete);
 
 module.exports = router;
