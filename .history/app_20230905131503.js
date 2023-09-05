52d60bb3e73bcc02a62ac81bc3c4fa82b423bb61
renanBotasse <renanBotasse@bitbucket.org>
2023-09-05 13:26:44 +0100

    backend complete

---
+++ .history/app_20230905131503.js
@@ -0,0 +1,66 @@
+//link express
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+// Config .env file
+dotenv.config();
+
+// Config ejs (view engine)
+app.set('view engine', 'ejs');
+
+// Create 
+app.use(express.static('public'));
+
+//start express
+const app = express();
+
+//MongoDB connection
+let mongoDB = 'mongodb+srv://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@cluster0.7796qkc.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(mongoDB);
+
+//MongoDB connection Confirmation
+mongoose.connection.on('connected', () => {
+	console.log('Connected to MongoDB');
+});
+
+//MongoDB connection Error
+mongoose.connection.on('error', (err) => {
+    console.log('Database Error' + err);
+});
+
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+
+//MIDDLEWARE START----------------------------------------------------------------
+//active json parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+//bad end point
+app.get('/', function(req, res) {
+  res.send("BAD END POINT");
+});
+
+//valid end point - start with /library
+const routes = require('./routes/libraryRoute');
+app.use('/library', routes);
+
+//error handler for middleware
+app.use(function(err, req, res, next) {
+	console.log(err);
+	//Change the status (res.status(422)
+	res.status(422).send({error: err.message});
+});
+
+//MIDDLEWARE END-------------------------------------------------------------------
+
+//create server port
+let port = 8000;
+
+//start server
+app.listen(process.env.PORT || port, () => {
+  console.log('Server running on port' + port);
+});
---
+++ .history/routes/libraryRoute_20230905122833.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+
+// import controller libraryController.js from /controllers
+const libraryController = require('../controllers/libraryController.js');
+
+// TEST ('GET') - route (url) to test (http://localhost:5000/library/test)
+router.get('/test', libraryController.test);
+
+// ALL ('GET') - route to get all books (http://localhost:5000/library/all)
+router.get('/all', libraryController.all)
+
+// ALL ('GET') - route to get one Book book (http://localhost:5000/library/oneBook)
+router.get('/oneBook', libraryController.oneBook)
+
+
+// CREATE ('POST')- route to add a book (http://localhost:5000/library/create)
+router.post('/create', libraryController.create);
+
+// UPDATE ('PUT')- route to update a book (http://localhost:5000/library/update/:id)
+router.put('/update/:id', libraryController.update);
+
+// DELETE ('DELETE') - route to delete a book (http://localhost:5000/library/delete/:id)
+router.delete('/delete/:id', libraryController.delete);
+
+module.exports = router;
