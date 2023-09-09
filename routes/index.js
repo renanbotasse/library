484247a87b0e5a5e2519b29288568e4c3f931f2d
renanBotasse <renanBotasse@bitbucket.org>
2023-09-09 11:52:45 +0100

    routes with auth and passport

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
+// importa a propriedade do objeto anónimo de '../config/auth'
+const { ensureAuthenticated } = require('../config/auth');
+
 const indexController = require("../controllers/indexController");
+
 // index-route (Home-page)
 router.get("/", indexController.index);
 // create-route
 router.get("/create", indexController.create);
+
+
+
+
 
 module.exports = router;
